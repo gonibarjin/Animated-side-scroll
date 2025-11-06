2d6c95fe85b98613ce83b76c2df54b189e8cfb43
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-11-06 17:18:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".container");
+const sections = gsap.utils.toArray(".container section");
+const texts = gsap.utils.toArray(".anim");
+const mask = document.querySelector(".mask");
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    end: "+=3000",
+  },
+});
+
+console.log(1 / (sections.length - 1));
+
+gsap.to(mask, {
+  width: "100%",
+  scrollTrigger: {
+    trigger: ".wrapper",
+    start: "top left",
+    scrub: 1,
+  },
+});
+
+sections.forEach((section) => {
+  let text = section.querySelectorAll(".anim");
+
+  if (text.length === 0) return;
+
+  gsap.from(text, {
+    y: -130,
+    opacity: 0,
+    duration: 2,
+    ease: "elastic",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: section,
+      containerAnimation: scrollTween,
+      start: "left center",
+      toggleActions: "play reverse play reverse",
+    },
+  });
+});
